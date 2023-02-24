5550f81ac9ea99507c717e9b842bc12ae11fdf32
Mario Campaz <mcampaz@hotmail.com>
2023-02-23 22:10:01 -0800

    [Add] Dnamic css styles

--- starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -27,9 +27,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler} onChange={clearStylesHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red': 'black'}}>Course Goal</label>
-        <input value={enteredValue} style={{borderColor: !isValid ? 'red': 'black', background: !isValid ? 'salmon': 'transparent'}} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input value={enteredValue} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
